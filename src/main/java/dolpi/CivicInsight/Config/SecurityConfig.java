176dc8fed285ed951133f2297d0d933781067cc3
Manas Rastogi <manasrastogi64@gmail.com>
2026-03-06 12:17:42 +0530

    Update CORS allowed origins in SecurityConfig
    Removed an empty string from the allowed origins in CORS configuration.

--- src/main/java/dolpi/CivicInsight/Config/SecurityConfig.java
+++ src/main/java/dolpi/CivicInsight/Config/SecurityConfig.java
@@ -71,6 +71,7 @@ public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Excepti
         CorsConfiguration configuration = new CorsConfiguration();
         // Frontend ke dono possible origins allow karein
         configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500", "https://civic-insight-2i2101yj3-avenger-manas-projects.vercel.app"));
+         configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500", " "));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "X-Requested-With"));
         configuration.setAllowCredentials(true);
