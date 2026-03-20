24c9eb8fcab7204808a9157364f64fd33eb1ba08
Manas Rastogi <manasrastogi64@gmail.com>
2026-03-20 19:41:26 +0530

    Update EmailService.java

--- src/main/java/dolpi/CivicInsight/Service/EmailService.java
+++ src/main/java/dolpi/CivicInsight/Service/EmailService.java
@@ -1,3 +1,13 @@
+
+package dolpi.CivicInsight.Service; 
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
 @Service
 public class EmailService {
 
@@ -12,7 +22,7 @@ public class EmailService {
             helper.setFrom("manasrastogi64@gmail.com");
             helper.setTo(to);
             helper.setSubject(subject);
-            helper.setText(body, true); // true = HTML support
+            helper.setText(body, true); 
             
             mailSender.send(message);
             System.out.println("Email sent to: " + to);
