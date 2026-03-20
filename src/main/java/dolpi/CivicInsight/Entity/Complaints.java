58db3085658c6b714788ac70bfe462739dc465da
Manas Rastogi <manasrastogi64@gmail.com>
2026-03-20 13:53:40 +0530

    Update Complaints.java

--- src/main/java/dolpi/CivicInsight/Entity/Complaints.java
+++ src/main/java/dolpi/CivicInsight/Entity/Complaints.java
@@ -1,3 +1,49 @@
+// package dolpi.CivicInsight.Entity;
+
+// import lombok.*;
+// import org.springframework.data.annotation.CreatedDate;
+// import org.springframework.data.annotation.Id;
+// import org.springframework.data.mongodb.core.mapping.Document;
+// import org.springframework.data.mongodb.core.mapping.Field;
+
+// import java.time.LocalDateTime;
+// import java.util.List;
+
+// @Getter
+// @Setter
+// @Document(collection = "complaint")
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class Complaints {
+
+//     @Id
+//     private String id;
+//     @NonNull
+//     private String Name;
+//     @NonNull
+//     private String city;
+//     @NonNull
+//     private String ward_number;
+//     @NonNull
+//     private String complaint;
+//     private String OfficerName;
+//     private String departmentName;
+//     @Field("officer_id")
+//     private String officerId;
+//     private String urgency;
+//     private List<String> listReport;
+
+//     @Field("user_id")
+//     private String userId;
+
+//     private String assignByAdmin;
+//     @CreatedDate
+//     private LocalDateTime createdAt;
+//     private boolean notified = false; // Important private String adminId; private String status;
+//     private String adminId;
+//     private String status;
+// }
+
 package dolpi.CivicInsight.Entity;
 
 import lombok.*;
@@ -5,26 +51,19 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Setter
 @Document(collection = "complaint")
-@NoArgsConstructor
-@AllArgsConstructor
-public class Complaints {
-
+@NoArgsConstructor                    
+public class Complaints {             
     @Id
     private String id;
-    @NonNull
-    private String Name;
-    @NonNull
+    private String Name;              
     private String city;
-    @NonNull
     private String ward_number;
-    @NonNull
     private String complaint;
     private String OfficerName;
     private String departmentName;
@@ -32,14 +71,12 @@ public class Complaints {
     private String officerId;
     private String urgency;
     private List<String> listReport;
-
     @Field("user_id")
     private String userId;
-
     private String assignByAdmin;
     @CreatedDate
     private LocalDateTime createdAt;
-    private boolean notified = false; // Important private String adminId; private String status;
+    private boolean notified = false;
     private String adminId;
     private String status;
 }
